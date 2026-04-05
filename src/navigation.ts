a1e261a142649574437d9ac2058fb87f3bd9c3f4
arbroadbent <amanda.broadbent@alumni.augsburg.edu>
2026-04-04 21:22:34 -0600

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -9,14 +9,7 @@ export const localePrefix = 'always'
 
 export const pathnames = {
   '/': '/',
-  '/about': '/about',
-  '/articles': '/articles',
-  '/articles/practicing-medicine-is-not-about-certainty': '/articles/practicing-medicine-is-not-about-certainty',
-  '/articles/medical-documentation-was-never-built': '/articles/medical-documentation-was-never-built',
-  '/articles/commitment-points': '/articles/commitment-points',
-  '/articles/capturing-multiplanar-thinking': '/articles/capturing-multiplanar-thinking',
-  '/articles/faithful-documentation': '/articles/faithful-documentation',
-  '/articles/past-the-hype': '/articles/past-the-hype',
+  '/about': '/about'
 } satisfies Pathnames<typeof locales>
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =
